28ecc3963537798e7361e6f843e067e289741bbb
Patrick McCrindle <35245519+pmccrind@users.noreply.github.com>
2020-04-14 12:42:51 -1000

    Started constructing Landing Page.
    Added all required image files.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Container, GridColumn, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        /* Outside to maintain consistent borders. */
+        <Container>
+          <Container>
+            <Image bordered fluid src='/images/medicine_ball.jpg'/>
+          </Container>
+          <Header textAlign='centered' as={'h1'}>Match based on what you are looking for!</Header>
+          <Grid divided = 'vertically'>
+            <Grid.Row columns={3}>
+              <Grid.Column>
+                Test 1
+              </Grid.Column>
+              <Grid.Column>
+                Test 2
+              </Grid.Column>
+              <Grid.Column>
+                Test 3
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Grid>
+            <GridColumn>
+              <Grid columns='equal' verticalAlign='middle' textAlign='center'>
+                <Image size='small' circular src="/images/meteor-logo.png"/>
+                <h1>Welcome to this template</h1>
+                <p>Now get to work and modify this app!</p>
+              </Grid>
+            </GridColumn>
+          </Grid>
+        </Container>
     );
   }
 }
