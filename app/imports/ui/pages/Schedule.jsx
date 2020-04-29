2e7a49594ad16444835cbc6973732210bb497a34
s-rathyen <38778256+s-rathyen@users.noreply.github.com>
2020-04-28 19:00:39 -1000

    #30: Fixing schedule page and style.css conflicts

--- app/imports/ui/pages/Schedule.jsx
+++ app/imports/ui/pages/Schedule.jsx
@@ -5,9 +5,10 @@ import { withTracker } from 'meteor/react-meteor-data';
 import swal from 'sweetalert';
 import {
   AutoForm, ErrorsField, HiddenField, SelectField, SubmitField,
-  TextField, LongTextField,
+  TextField, LongTextField, DateField
 } from 'uniforms-semantic';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import MultiSelectField from '../forms/controllers/MultiSelectField';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
 import Event from '/imports/ui/components/Event';
@@ -16,15 +17,17 @@ import { Events } from '../../api/event/Events';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const formSchema = new SimpleSchema({
-  date: String,
-  time: String,
+  date: Date,
   type: {
     type: String,
     allowedValues: ['Run', 'Hike', 'Walk', 'Lift', 'Other'],
     defaultValue: 'Walk',
   },
   location: String,
-  associated: String,
+  associated: {
+    type: String,
+    optional: true,
+  },
   notes: String,
 });
 
@@ -33,9 +36,9 @@ class Schedule extends React.Component {
 
   /** On successful submit, insert the data. */
   submit(data, formRef) {
-    const { date, time, type, location, associated, notes } = data;
+    const { date, type, location, associated, notes } = data;
     const owner = Meteor.user().username;
-    Events.insert({ date, time, type, location, associated, notes, owner },
+    Events.insert({ date, type, location, associated, notes, owner },
         (error) => {
           if (error) {
             swal('Error', error.message, 'error');
@@ -58,27 +61,46 @@ class Schedule extends React.Component {
         <Grid container>
           <Divider hidden/>
           <Grid.Row>
-            <Grid.Column width={4}>
+            <Grid.Column textAlign='center' width={5}>
+              <Divider hidden/>
               <Segment fixed inverted className='scheduleBar'>
                 <Header as='h3' textAlign='center'>SCHEDULE A NEW EVENT</Header>
-                <AutoForm ref={ref => { fRef = ref; }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
-                  <TextField name='date'/>
-                  <TextField name='time'/>
-                  <SelectField name='type' label='Event Type'/>
-                  <TextField name='location'/>
-                  <TextField name='associated' label='Include a Friend'/>
-                  <LongTextField name='notes'/>
-                  <SubmitField className='editProfileButton' value='Submit Changes'/>
+                <AutoForm ref={ref => {
+                  fRef = ref;
+                }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
+                  <DateField className='schedLabel' name='date' label='Date and Time'/>
+                  <SelectField className='schedLabel' name='type' label='Event Type'/>
+                  <TextField className='schedLabel' name='location'/>
+                  <TextField className='schedLabel' name='associated' label='Include a Friend'/>
+                  <LongTextField className='schedLabel' name='notes'/>
+                  <Divider hidden/>
+                  <SubmitField className='scheduleButton' value='CREATE EVENT'/>
                   <ErrorsField/>
                 </AutoForm>
+                <Divider hidden/>
               </Segment>
             </Grid.Column>
-            <Grid.Column width={12}>
+            <Grid.Column width={11}>
               <Divider horizontal>
                 <Header as='h1' textAlign='center'>Upcoming Events</Header>
               </Divider>
               <Card.Group centered>
                 {this.props.events.map((event, index) => <Event
+                    key={index}
+                    event={event}
+                    Events={Events}
+                />)
+                }
+              </Card.Group>
+
+              <Divider hidden/>
+              <Divider hidden/>
+
+              <Divider horizontal>
+                <Header as='h1' textAlign='center'>Past Events</Header>
+              </Divider>
+              <Card.Group className='pastCards' centered>
+                {this.props.events2.map((event, index) => <Event
                     key={index}
                     event={event}
                     Events={Events}
@@ -95,15 +117,23 @@ class Schedule extends React.Component {
 
 Schedule.propTypes = {
   events: PropTypes.array.isRequired,
+  events2: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components.
  https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
+  const today = new Date();
+  today.setHours(today.getHours() - 10);
   const subscription = Meteor.subscribe('Events');
   return {
-    events: Events.find({}).fetch(),
+    events: Events.find({
+      date: { $gte: today },
+    }, { sort: { date: 1 } }).fetch(),
+    events2: Events.find({
+      date: { $lt: today },
+    }, { sort: { date: -1 } }).fetch(),
     ready: subscription.ready(),
   };
 })(Schedule);
