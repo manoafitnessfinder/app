9f353d94ec8c8254d7dec930e4c727c86d0a6b24
Patrick McCrindle <35245519+pmccrind@users.noreply.github.com>
2020-05-01 09:54:41 -1000

    Final Size.
    Added padding to the top of image.

--- app/imports/ui/pages/Friends.jsx
+++ app/imports/ui/pages/Friends.jsx
@@ -18,7 +18,7 @@ class FriendsPage extends React.Component {
     return (
         <Container>
           <Container className = 'imagePadding'>
-            <Image style={ { width: '980px', height: '250px' }} centered src="/images/listFriend.PNG"/>
+            <Image style={ { width: '987px', height: '250px' }} centered src="/images/listFriend.PNG"/>
           </Container>
           <Card.Group centered>
             {this.props.profiles.map((profile, index) => <FriendsList key={index} profile={profile}/>)}
