05ab24e78d296106e137d34c8e639201a07f4a31
Christine Uehara <42711372+cyuehara@users.noreply.github.com>
2020-05-02 10:01:19 -1000

    Sign-up functionality added
    User should be able to signup an acount from the about page

--- app/imports/ui/pages/About.jsx
+++ app/imports/ui/pages/About.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
-import { Container, Image, Grid, Header, Form, Button, Input, List, Icon } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import swal from 'sweetalert';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Redirect } from 'react-router-dom';
+import { Container, Image, Grid, Header, Form, List, Icon, Checkbox, Message } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Accounts } from 'meteor/accounts-base';
+import { Profiles } from '../../api/profile/Profile';
 
-/** A simple static component to render some text for the landing page. */
+/**
+ * Signup component is similar to signin component, but we create a new user instead.
+ */
 class About extends React.Component {
+  /** Initialize state fields. */
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', error: '', redirectToReferer: false };
+  }
+
+  /** Update the form controls each time the user interacts with them. */
+  handleChange = (e, { name, value }) => {
+    this.setState({ [name]: value });
+  }
+
+  /** Handle Signup submission. Create user account and a profile entry, then redirect to the home page. */
+  submit = () => {
+    const { name, email, password } = this.state;
+    const owner = this.state.email;
+    Accounts.createUser({ email, username: email, password }, (err) => {
+      if (err) {
+        this.setState({ error: err.reason });
+      }
+    });
+    const interests = ['Walking'];
+    const age = 18;
+    const image = '';
+    const description = '';
+    const gender = 'Undefined';
+    const seeking = 'No Preference';
+    const level = 'Beginner';
+    Profiles.insert({ name, interests, age, image, description, gender, seeking, level, owner }, err => {
+      if (err) {
+        swal('Error', err.message, 'error');
+      } else {
+        this.setState({ error: '', redirectToReferer: true });
+      }
+    });
+  }
+
+  /** Display the signup form. Redirect to add page after successful registration and login. */
   render() {
+    const { from } = this.props.location.state || { from: { pathname: /profile/ } };
+    // if correct authentication, redirect to from: page instead of signup screen
+    if (this.state.redirectToReferer) {
+      return <Redirect to={from}/>;
+    }
     return (
         <div className="aboutBackground">
         <Container>
@@ -84,30 +135,49 @@ class About extends React.Component {
                   </Header.Subheader>
                 </Header>
                 <Grid.Row>
-                  <Form>
+                  <Form onSubmit={this.submit}>
+                    <Container>
                     <Form.Group widths='equal'>
-                      <Form.Field
-                          id='form-input-control-first-name'
-                          control={Input}
-                          placeholder='First name'
-                      />
-                      <Form.Field
-                          id='form-input-control-last-name'
-                          control={Input}
-                          placeholder='Last name'
+                      <Form.Input fluid label='Name'
+                                  name="name"
+                                  type="name"
+                                  placeholder='Your name here.'
+                                  onChange={this.handleChange}
                       />
                     </Form.Group>
-                    <Form.Field
-                        id='form-input-control-error-email'
-                        control={Input}
-                        placeholder='Please enter email address'
-                    />
-                    <Form.Field
-                        id='form-button-control-public'
-                        control={Button}
-                        content='Confirm'
-                    />
+                    <Form.Group widths='equal'>
+                      <Form.Input fluid
+                                  label="Email"
+                                  name="email"
+                                  type="email"
+                                  placeholder="UH E-mail address"
+                                  onChange={this.handleChange}
+                      />
+                      <Form.Input
+                          label="Password"
+                          name="password"
+                          type="password"
+                          placeholder="Password"
+                          onChange={this.handleChange}
+                      />
+                    </Form.Group>
+                    <Form.Field align='center'>
+                      <Checkbox label='I agree to the Terms and Conditions'/>
+                    </Form.Field>
+                    <Form.Field align='center'>
+                      <Form.Button content="Set Up Profile"/>
+                    </Form.Field>
+                    </Container>
                   </Form>
+                  {this.state.error === '' ? (
+                      ''
+                  ) : (
+                      <Message
+                          error
+                          header="There was an error is your registration. Try again or contact us."
+                          content={this.state.error}
+                      />
+                  )}
                 </Grid.Row>
               </Grid>
             </div>
@@ -117,5 +187,18 @@ class About extends React.Component {
     );
   }
 }
+/** Ensure that the React Router location object is available in case we need to redirect. */
+About.propTypes = {
+  profiles: PropTypes.array.isRequired,
+  location: PropTypes.object,
+  ready: PropTypes.bool.isRequired,
+};
 
-export default About;
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  const subscription = Meteor.subscribe('Profiles');
+  return {
+    profiles: Profiles.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(About);
