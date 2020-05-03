6b7a4530a864e3d411cf920076df665b67d22c89
Patrick McCrindle <35245519+pmccrind@users.noreply.github.com>
2020-05-02 18:42:05 -1000

    Made required changes for profiles to only load once.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -41,11 +41,13 @@ if (Profiles.find().count() === 0) {
  * For more info on assets, see https://docs.meteor.com/api/assets.html
  * User count check is to make sure we don't load the file twice, which would generate errors due to duplicate info.
  */
-if ((Meteor.settings.loadAssetsFile) && (Meteor.users.find().count() < 200)) {
+if ((Meteor.settings.loadAssetsFile) && (Meteor.users.find().count() < 105)) {
   const assetsFileName = 'data.json';
   console.log(`Loading data from private/${assetsFileName}`);
   const jsonData = JSON.parse(Assets.getText(assetsFileName));
-  jsonData.profiles.map(profile => addProfile(profile));
+  if (Profiles.find().count() < 100) {
+    jsonData.profiles.map(profile => addProfile(profile));
+  }
 }
 
 function addEvents(data) {
