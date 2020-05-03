44b35dbc250f41887d3c92408808bdc2ad87616d
njanders9124 <60155312+njanders9124@users.noreply.github.com>
2020-05-02 17:10:16 -1000

    Created backend for FriendEvent page
    test

---
+++ app/imports/ui/components/FriendEvent.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Card, Icon, Header } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { Link } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import moment from 'moment';
+import 'moment-timezone';
+import { Friends } from '../../api/friend/Friend';
+import { Profiles } from '../../api/profile/Profile';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Event extends React.Component {
+  removeItem(docID) {
+    this.props.Events.remove(docID);
+  }
+
+  render() {
+    if (Friends.findOne({ friendEmail: this.props.event.owner })) {
+      return (
+          <Card color='green' centered>
+            <Card.Content>
+              <Header as='h3'>
+                {moment(this.props.event.date).add(10, 'hours').format('MM/DD/YYYY')}<br/>
+                @ {moment(this.props.event.date).add(10, 'hours').format('hh:mm A')}<br/>
+              </Header>
+            </Card.Content>
+            <Card.Content>
+              <Icon name='thumbtack'/> {this.props.event.type}<br/>
+              <Icon name='location arrow'/> {this.props.event.location}<br/>
+              <Icon name='user plus'/> {this.props.event.associated}<br/>
+            </Card.Content>
+            <Card.Content>
+              <b>NOTES</b><br/>
+              {this.props.event.notes}
+            </Card.Content>
+            <Card.Content extra textAlign='right'>
+              <Icon name='edit'/><Link to={`/editevent/${this.props.event._id}`}>Edit</Link>
+              <Icon name='trash'/>
+              <a onClick={() => this.removeItem(this.props.event._id)}>Delete</a>
+            </Card.Content>
+          </Card>
+      );
+    }
+    return (<h3></h3>);
+  }
+}
+
+/** Require a document to be passed to this component. */
+Event.propTypes = {
+  event: PropTypes.object.isRequired,
+  Events: PropTypes.object.isRequired,
+  friends: PropTypes.array.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withTracker(() => {
+  const subscriptionFriends = Meteor.subscribe('Friends');
+  const subscriptionsProfile = Meteor.subscribe('Profiles');
+  return {
+    friends: Friends.find({}).fetch(),
+    profile: Profiles.find({}).fetch(),
+    ready: subscriptionFriends.ready() && subscriptionsProfile.ready(),
+  };
+})(Event);
--- app/imports/ui/pages/FriendFeed.jsx
+++ app/imports/ui/pages/FriendFeed.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Loader, Segment, Divider, Header, Card, Container, Image } from 'semantic-ui-react';
+import { Grid, Loader, Segment, Divider, Header, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
+import swal from 'sweetalert';
+import {
+  AutoForm, ErrorsField, SelectField, SubmitField,
+  TextField, LongTextField, DateField,
+} from 'uniforms-semantic';
 import PropTypes from 'prop-types';
+import SimpleSchema from 'simpl-schema';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
-import Event from '/imports/ui/components/Event';
-import { _ } from 'meteor/underscore';
+import FriendEvent from '/imports/ui/components/FriendEvent';
 import { Events } from '../../api/event/Events';
-import FriendsList from '../components/Friends';
-import { Profiles } from '../../api/profile/Profile.js';
-import { Friends } from '../../api/friend/Friend.js';
 
+/** Create a schema to specify the structure of the data to appear in the form. */
+const formSchema = new SimpleSchema({
+  date: Date,
+  type: {
+    type: String,
+    allowedValues: ['Run', 'Hike', 'Walk', 'Lift', 'Other'],
+    defaultValue: 'Walk',
+  },
+  location: String,
+  associated: {
+    type: String,
+    optional: true,
+  },
+  notes: String,
+});
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class FriendFeed extends React.Component {
+class Schedule extends React.Component {
+
+  /** On successful submit, insert the data. */
+  submit(data, formRef) {
+    const { date, type, location, associated, notes } = data;
+    const owner = Meteor.user().username;
+    console.log(`the owner of this event is ${owner}`);
+    Events.insert({ date, type, location, associated, notes, owner },
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+          } else {
+            swal('Success', 'Item added successfully', 'success');
+            formRef.reset();
+          }
+        });
+  }
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -21,35 +55,78 @@ class FriendFeed extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    let fRef = null;
     return (
-        <Container>
-          <Container className = 'imagePadding'>
-            <Image style={ { width: '987px', height: '250px' }} centered src="/images/listFriend.PNG"/>
-          </Container>
-          <Card.Group centered>
-            {this.props.friendEv.map((event, index) => <Event key={index} event={event}/>)}
-          </Card.Group>
-        </Container>
+        <Grid container>
+          <Divider hidden/>
+          <Grid.Row>
+            <Grid.Column textAlign='center' width={5}>
+              <Divider hidden/>
+              <Segment fixed inverted className='scheduleBar'>
+                <Header as='h3' textAlign='center'>SCHEDULE A NEW EVENT</Header>
+                <AutoForm ref={ref => {
+                  fRef = ref;
+                }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
+                  <DateField className='schedLabel' name='date' label='Date and Time'/>
+                  <SelectField className='schedLabel' name='type' label='Event Type'/>
+                  <TextField className='schedLabel' name='location'/>
+                  <TextField className='schedLabel' name='associated' label='Include a Friend'/>
+                  <LongTextField className='schedLabel' name='notes'/>
+                  <Divider hidden/>
+                  <SubmitField className='scheduleButton' value='CREATE EVENT'/>
+                  <ErrorsField/>
+                </AutoForm>
+                <Divider hidden/>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={11}>
+              <Divider horizontal>
+                <Header as='h1' textAlign='center'>Upcoming Events</Header>
+              </Divider>
+              <Card.Group centered>
+                {this.props.events.map((event, index) => <FriendEvent key={index} event={event}/>)}
+              </Card.Group>
+
+              <Divider hidden/>
+              <Divider hidden/>
+
+              <Divider horizontal>
+                <Header as='h1' textAlign='center'>Past Events</Header>
+              </Divider>
+              <Card.Group className='pastCards' centered>
+                {this.props.events2.map((event, index) => <FriendEvent
+                    key={index}
+                    event={event}
+                />)
+                }
+              </Card.Group>
+            </Grid.Column>
+          </Grid.Row>
+          <Divider hidden/>
+        </Grid>
     );
   }
 }
 
-FriendFeed.propTypes = {
-  friendEv: PropTypes.array.isRequired,
+Schedule.propTypes = {
+  events: PropTypes.array.isRequired,
+  events2: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components.
  https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
-  const sub1 = Meteor.subscribe('Friends');
-  const sub2 = Meteor.subscribe('AllEvents');
-  var friendNames = Friends.find({}).fetch();
-  var friend2 = Friends.find({}, { contactId: 1 }).fetch();
-  console.log(_.pluck(friend2, 'contactId'));
-  console.log(friendNames);
+  const today = new Date();
+  today.setHours(today.getHours() - 10);
+  const subscription = Meteor.subscribe('Events');
   return {
-    friendEv: Events.find({ owner: { $in: friend2 }}).fetch(),
-    ready: sub1.ready(),
+    events: Events.find({
+      date: { $gte: today },
+    }, { sort: { date: 1 } }).fetch(),
+    events2: Events.find({
+      date: { $lt: today },
+    }, { sort: { date: -1 } }).fetch(),
+    ready: subscription.ready(),
   };
-})(FriendFeed);
+})(Schedule);
