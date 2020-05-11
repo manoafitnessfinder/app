f140f6b7f43afefa94920783a90cb85dc1ae7f57
s-rathyen <38778256+s-rathyen@users.noreply.github.com>
2020-05-10 17:56:51 -1000

    #71 Fixing grammatical errors on landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Image, Container, Header, Segment, Button, Reveal, Icon, List } from 'semantic-ui-react';
+import { Divider, Grid, Image, Container, Header, Segment, Button, Reveal, Icon, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -13,21 +13,22 @@ class Landing extends React.Component {
           </Container>
           <Header className = 'landingPage' textAlign='centered' as={'h1'}>
             5 Simple Steps to your Fitness Goals!</Header>
-          <Grid>
+          <Grid verticalAlign='middle'>
             <Grid.Row columns={3}>
-              <Grid.Column>
+              <Grid.Column verticalAlign='middle'>
                 <Reveal animated='move left' instant>
                   <Reveal.Content visible>
                     <Image fluid src='/images/Landing2.jpg'/>
                   </Reveal.Content>
-                  <Reveal.Content hidden>
+                  <Reveal.Content hidden verticalAlign='middle'>
                     <br/>
                     <Header className = 'landingPage' textAlign='centered' as={'h1'}>
-                      Schedule an event with a friend today!<br/>
-                      <Button as={Link} to="/signup" animated size="large">
-                        <Button.Content visible>Start Your Workout</Button.Content>
+                      Schedule an event with a friend today!
+                      <Divider hidden/>
+                      <Button className='grnBtn' as={Link} to="/signup" animated size="large">
+                        <Button.Content visible>Create a Workout</Button.Content>
                         <Button.Content hidden>
-                          <Icon name='arrow right'/>
+                          <Icon className='iWht' name='arrow right'/>
                         </Button.Content>
                       </Button>
                     </Header>
@@ -35,31 +36,25 @@ class Landing extends React.Component {
                 </Reveal>
               </Grid.Column>
               <Grid.Column textAlign='center'>
-                <br/>
                 <List as='h2' size='large'>
                   <List.Item>
-                    <Icon name='user' />
-                    Create an account
+                    <Icon name='user' /> Create an account
                   </List.Item>
                   <br/>
                   <List.Item>
-                    <Icon name='edit' />
-                    Edit user profile
+                    <Icon name='edit' /> Edit your profile
                   </List.Item>
                   <br/>
                   <List.Item>
-                    <Icon name='search' />
-                    Find Fitness Friends
+                    <Icon name='search' /> Find Fitness Friends
                   </List.Item>
                   <br/>
                   <List.Item>
-                    <Icon name='calendar' />
-                    Create schedule workouts
+                    <Icon name='calendar' /> Schedule new workouts
                   </List.Item>
                   <br/>
                   <List.Item>
-                    <Icon name='feed' />
-                    Track your friends events
+                    <Icon name='feed' /> See your friends' events
                   </List.Item>
                 </List>
               </Grid.Column>
@@ -71,7 +66,7 @@ class Landing extends React.Component {
                   <Reveal.Content hidden>
                     <br/>
                     <Header className = 'landingPage' textAlign='centered' as={'h1'}>
-                      The Freshmen Fifteen could happen to you, get started today.
+                      Avoid the freshman fifteen! Get started right away.
                     </Header>
                   </Reveal.Content>
                 </Reveal>
@@ -79,11 +74,11 @@ class Landing extends React.Component {
             </Grid.Row>
           </Grid>
           <Header className = 'landingPage' textAlign='centered' as={'h1'}>
-           Work towards your better self!</Header>
+           Work for a better you. </Header>
           <Container className='landing_bottom_image'>
             <Segment basic floated='right' compact textAlign='center'>
-              <Button as={Link} to="/signup" animated className="JoinButton" size="large">
-                <Button.Content visible>Join Now</Button.Content>
+              <Button className='scheduleButton' as={Link} to="/signup" animated size="large">
+                <Button.Content visible>JOIN NOW</Button.Content>
                 <Button.Content hidden>
                   <Icon name='arrow right'/>
                 </Button.Content>
