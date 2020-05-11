bb10be17736f086515bafb2063b2b17158b527ac
Lsrock <42789764+Lsrock@users.noreply.github.com>
2020-05-10 19:18:11 -1000

    Needs to be updated

--- app/imports/ui/components/Note.jsx
+++ app/imports/ui/components/Note.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Feed } from 'semantic-ui-react';
+import {Feed, Menu} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import {NavLink, withRouter} from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Note extends React.Component {
@@ -11,7 +11,9 @@ class Note extends React.Component {
           <Feed.Event inverted>
             <Feed.Label image={this.props.note.image}/>
             <Feed.Content>
-              <Feed.Summary>
+              <Feed.Summary as={NavLink}
+                            activeClassName=""
+                            exact to={`/User/${this.props.note._id}`}>
                 {this.props.note.madeBy}
                 <Feed.Date content={this.props.note.createdAt.toLocaleDateString('en-US')}/>
               </Feed.Summary>
