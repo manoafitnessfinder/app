995778621c26504a15ca79a2acba20f3be5e76bb
Lsrock <42789764+Lsrock@users.noreply.github.com>
2020-05-11 15:07:41 -1000

    Comments finished
    Image and name now links to user profile

--- app/imports/ui/components/Note.jsx
+++ app/imports/ui/components/Note.jsx
@@ -5,33 +5,38 @@ import {NavLink, withRouter} from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Note extends React.Component {
-  render() {
-    if (this.props.profile === this.props.note.owner) {
-      return (
-          <Feed.Event inverted>
-            <Feed.Label image={this.props.note.image}/>
-            <Feed.Content>
-              <Feed.Summary as={NavLink}
+    render() {
+        if (this.props.profile === this.props.note.owner) {
+            return (
+                <Feed.Event inverted>
+                    <Feed.Label
+                        image={this.props.note.image}
+                        as={NavLink}
+                        activeClassName=""
+                        exact to={`/User/${this.props.note.contactId}`}/>
+                    <Feed.Content>
+                        <Feed.Summary
+                            as={NavLink}
                             activeClassName=""
-                            exact to={`/User/${this.props.note._id}`}>
-                {this.props.note.madeBy}
-                <Feed.Date content={this.props.note.createdAt.toLocaleDateString('en-US')}/>
-              </Feed.Summary>
-              <Feed.Summary>
-                {this.props.note.note}
-              </Feed.Summary>
-            </Feed.Content>
-          </Feed.Event>
-      );
+                            exact to={`/User/${this.props.note.contactId}`}>
+                            {this.props.note.madeBy}
+                            <Feed.Date content={this.props.note.createdAt.toLocaleDateString('en-US')}/>
+                        </Feed.Summary>
+                        <Feed.Summary>
+                            {this.props.note.note}
+                        </Feed.Summary>
+                    </Feed.Content>
+                </Feed.Event>
+            );
+        }
+        return (<h3></h3>);
     }
-    return (<h3></h3>);
-  }
 }
 
 /** Require a document to be passed to this component. */
 Note.propTypes = {
-  note: PropTypes.object.isRequired,
-  profile: PropTypes.string.isRequired,
+    note: PropTypes.object.isRequired,
+    profile: PropTypes.string.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
