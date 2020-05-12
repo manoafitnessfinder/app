6fc049bae21cc26246257af7fc7b4b69cfe04183
Lsrock <42789764+Lsrock@users.noreply.github.com>
2020-05-11 21:07:02 -1000

    Contact image
    Switched out contact image

--- app/imports/ui/pages/Contact.jsx
+++ app/imports/ui/pages/Contact.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Icon } from 'semantic-ui-react';
 
 class Contact extends React.Component {
 
   render() {
-      const gridStyle = { height: '800px' };
+      const gridStyle = { height: '550px', paddingTop: '100px' };
       return (
-        <Grid container verticalAlign="middle" style={gridStyle}>
-          <Grid.Row>
+          <Grid style={gridStyle} centered stackable columns='2' container>
             <Grid.Column>
-              <Image src="/images/Contact.jpg" />
+              <Image src="/images/contactP.jpg" />
             </Grid.Column>
-          </Grid.Row>
+            <Grid.Column textAlign="center" style={{ color: '#6D9B7C' }}>
+                <h1>Have any issues, comments, or suggestions?</h1>
+                <hr/>
+                <h2>We would love to hear from you!</h2>
+                <h3>email us at:</h3>
+                <h3>manoafitnessfinder@gmail.com</h3>
+            </Grid.Column>
         </Grid>
     );
   }
